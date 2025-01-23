42a9d7dba9a103f3ba2d81b2c87f495b66fe9411
Stephen Asembo <165886904+Stephenasembo@users.noreply.github.com>
2025-01-23 16:27:57 +0300

    Refactor The Capitalize Function To Pass The Test
    Refactoring comes last in our test driven development approach

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 function capitalize(string) {
-  return string;
+  const stringArr = string.split('');
+  stringArr[0] = stringArr[0].toUpperCase();
+  return stringArr.join('');
 }
 
 export default capitalize;
